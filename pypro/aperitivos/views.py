af82c765795106428f4d778b02952fbcde1e760e
Nilton Pimentel <niltonpimentel02@gmail.com>
2020-08-29 16:45:33 -0300

    Criada página de indices de vídeos aperitivos, Acesso a atributos através de contexto de template, Criação e acesso de get_absolute_url para video, Extraido Rodapé utilizando composição de templates (include)
    Close #93

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
+from django.urls import reverse
+
+
+class Video:
+    def __init__(self, slug, titulo, vimeo_id):
+        self.slug = slug
+        self.titulo = titulo
+        self.vimeo_id = vimeo_id
+
+    def get_absolute_url(self):
+        return reverse('aperitivos:video', args=(self.slug,))
+
+
+videos = [
+    Video('motivacao', 'Video Aperitivo: Motivação', 450627819),
+    Video('instalacao-windows', 'Instalação Windows', 452882916),
+]
+
+videos_dct = {v.slug: v for v in videos}
+
+
+def indice(resquest):
+    return render(resquest, 'aperitivos/indice.html', context={'videos': videos})
 
 
 def video(request, slug):
-    videos = {
-        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 450627819},
-        'instalacao-windows': {'titulo': 'Instalação Windows', 'vimeo_id': 452882916},
-    }
-    video = videos[slug]
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
